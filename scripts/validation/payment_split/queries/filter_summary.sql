bbb8800b77d6fe84d549394d28476f409b1a9d13
Benjamin Rains <rains.bp@gmail.com>
2025-02-24 21:06:35 -0500

    Removed CTEs

--- scripts/validation/payment_split/queries/filter_summary.sql
+++ scripts/validation/payment_split/queries/filter_summary.sql
@@ -4,38 +4,50 @@
 -- uses FilterStats CTE from ctes.sql
 -- uses PaymentFilterDiagnostics CTE from ctes.sql
 
+WITH FilterDiagnosticMetrics AS (
+    -- Calculate metrics by filter reason that aren't in FilterStats
+    SELECT 
+        filter_reason,
+        AVG(split_count) as avg_splits,
+        MIN(PayAmt) as min_amount,
+        MAX(PayAmt) as max_amount
+    FROM PaymentFilterDiagnostics
+    GROUP BY filter_reason
+)
+
 SELECT 
-    filter_reason,
-    payment_count,
-    percentage,
-    total_amount,
-    avg_amount,
-    -- Diagnostic metrics (from diagnostic.sql)
-    avg_splits,
-    min_amount,
-    max_amount,
+    fs.filter_reason,
+    fs.payment_count,
+    fs.percentage,
+    fs.total_amount,
+    fs.avg_amount,
+    -- Diagnostic metrics from the supplementary CTE
+    fdm.avg_splits,
+    fdm.min_amount,
+    fdm.max_amount,
     -- Validation flags
     CASE 
-        WHEN filter_reason = 'Zero Amount' AND percentage != 13.1 
+        WHEN fs.filter_reason = 'Zero Amount' AND fs.percentage != 13.1 
             THEN 'Unexpected: Should be 13.1%'
-        WHEN filter_reason = 'High Split Count' AND percentage != 0.2 
+        WHEN fs.filter_reason = 'High Split Count' AND fs.percentage != 0.2 
             THEN 'Unexpected: Should be 0.2%'
-        WHEN filter_reason = 'Reversal' AND percentage != 0.6 
+        WHEN fs.filter_reason = 'Reversal' AND fs.percentage != 0.6 
             THEN 'Unexpected: Should be 0.6%'
-        WHEN filter_reason = 'No Insurance' AND percentage != 38.8 
+        WHEN fs.filter_reason = 'No Insurance' AND fs.percentage != 38.8 
             THEN 'Unexpected: Should be 38.8%'
-        WHEN filter_reason = 'No Procedures' AND percentage != 4.6 
+        WHEN fs.filter_reason = 'No Procedures' AND fs.percentage != 4.6 
             THEN 'Unexpected: Should be 4.6%'
-        WHEN filter_reason = 'Normal Payment' AND percentage != 47.2 
+        WHEN fs.filter_reason = 'Normal Payment' AND fs.percentage != 47.2 
             THEN 'Unexpected: Should be 47.2%'
         ELSE 'OK'
     END as validation_check,
     -- Additional metrics
-    complex_split_count,
-    large_payment_count,
-    simple_payment_count,
-    high_ratio_count,
-    oversplit_claim_count,
+    fs.complex_split_count,
+    fs.large_payment_count,
+    fs.simple_payment_count,
+    fs.high_ratio_count,
+    fs.oversplit_claim_count,
     'filter_validation' as report_type
-FROM FilterStats
-ORDER BY payment_count DESC;
+FROM FilterStats fs
+JOIN FilterDiagnosticMetrics fdm ON fs.filter_reason = fdm.filter_reason
+ORDER BY fs.payment_count DESC;
