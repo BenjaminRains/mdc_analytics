1d0f0e5394be926edd0aea71f4f66ef33d29cdc8
Benjamin Rains <rains.bp@gmail.com>
2025-02-24 21:05:31 -0500

    fixed p.PayDate to reflect all of 2024 range

--- scripts/validation/payment_split/queries/ctes.sql
+++ scripts/validation/payment_split/queries/ctes.sql
@@ -468,7 +468,7 @@ PaymentDailyDetails AS (
     JOIN paysplit ps ON p.PayNum = ps.PayNum
     JOIN claimproc cp ON ps.ProcNum = cp.ProcNum
     JOIN claim c ON cp.ClaimNum = c.ClaimNum
-    WHERE p.PayDate >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)
+    WHERE p.PayDate >= '2024-01-01' AND p.PayDate < '2025-01-01'
 ),
 
 -- 19. FilterStats: Compute summary statistics for each payment filter category.
