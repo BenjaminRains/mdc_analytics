f4d827e9f8642bccce1134180ce256b576e2d205
Benjamin Rains <rains.bp@gmail.com>
2025-02-26 11:25:47 -0500

    added date filter for dynamic assembly process

--- scripts/validation/procedurelog/queries/ctes/appointment_details.sql
+++ scripts/validation/procedurelog/queries/ctes/appointment_details.sql
@@ -2,6 +2,7 @@
 -- Provides appointment information within date range
 -- Used for joining procedures to appointments and tracking appointment status
 -- dependent CTEs: None
+-- Date filter: 2024-01-01 to 2025-01-01
 AppointmentDetails AS (
     SELECT
         a.AptNum,
