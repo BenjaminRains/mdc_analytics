d65ca8c73477c18892820f9a888d61813befb585
Benjamin Rains <rains.bp@gmail.com>
2025-02-26 23:00:36 -0500

    fixed dependent CTEs comment to include actual file names in CTEs, updated date filter to include date variable string arguments. add date filter comment.

--- scripts/validation/procedurelog/queries/ctes/appt_production.sql
+++ scripts/validation/procedurelog/queries/ctes/appt_production.sql
@@ -1,11 +1,13 @@
 -- Sum the procedure fees per appointment (production)
+-- Date filter: 2024-01-01 to 2025-01-01
+-- Dependent CTEs:
 appt_production AS (
   SELECT 
     AptNum,
     ProvNum,
     SUM(ProcFee) AS appt_production
   FROM procedurelog
-  WHERE ProcDate >= '2024-01-01' AND ProcDate < '2025-01-01'
+  WHERE ProcDate >= '{{START_DATE}}' AND ProcDate < '{{END_DATE}}'
     AND ProcStatus = 2
   GROUP BY AptNum, ProvNum
 )
