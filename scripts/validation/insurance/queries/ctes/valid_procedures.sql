e74b4dee7d202b6749466ea7aa75bfcd6a50f4ad
Benjamin Rains <rains.bp@gmail.com>
2025-02-27 21:40:06 -0500

    CTE for procedures with claims in date range

---
+++ scripts/validation/insurance/queries/ctes/valid_procedures.sql
@@ -0,0 +1,39 @@
+-- Get procedures with claims in date range
+-- Date range: 2024-01-01 to 2025-01-01
+-- Dependencies:
+
+ValidProcedures AS (
+    SELECT 
+        pl.ProcNum,
+        pl.PatNum,
+        pl.ProcDate,
+        pl.ProcFee,
+        pl.CodeNum,
+        cp.PlanNum,
+        cp.ClaimNum,
+        cp.InsPayEst,
+        cp.InsPayAmt,
+        cp.WriteOff,
+        cp.DedApplied,
+        cp.Status as ClaimStatus,
+        cp.DateCP as ClaimPaymentDate,
+        CASE 
+            WHEN cp.DateCP IS NOT NULL 
+            AND DATEDIFF(cp.DateCP, pl.ProcDate) BETWEEN 0 AND 365
+            THEN DATEDIFF(cp.DateCP, pl.ProcDate)
+        END as days_to_payment
+    FROM procedurelog pl 
+    FORCE INDEX (idx_ml_proc_core)
+    INNER JOIN claimproc cp 
+    FORCE INDEX (idx_ml_claimproc_core)
+    ON pl.ProcNum = cp.ProcNum
+    INNER JOIN insplan ip 
+    ON cp.PlanNum = ip.PlanNum
+    INNER JOIN ValidCarriers vc 
+    ON ip.CarrierNum = vc.CarrierNum
+    CROSS JOIN DateRange d
+    WHERE pl.ProcDate BETWEEN d.start_date AND d.end_date
+        AND pl.ProcStatus = 2  -- Complete
+        AND pl.ProcFee > 0     -- Only procedures with fees
+        AND cp.Status != 7     -- Exclude reversed claims
+)
