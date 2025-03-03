c5387c322d67c0f8f2f08bfb9335de4ab2150e52
Benjamin Rains <rains.bp@gmail.com>
2025-03-03 12:33:55 -0500

    added date range CTE to payment_split dir

---
+++ scripts/validation/payment_split/queries/ctes/date_range.sql
@@ -0,0 +1,8 @@
+-- Set date range variables to ensure consistency
+-- Date range: 2024-01-01 to 2025-01-01
+-- Dependencies:
+WITH DateRange AS (
+    SELECT 
+        '2024-01-01' as start_date,
+        '2025-01-01' as end_date
+)
