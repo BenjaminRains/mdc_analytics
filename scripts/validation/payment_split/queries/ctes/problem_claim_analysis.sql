c2073b1c6badb53c84483df884b14def67165e27
Benjamin Rains <rains.bp@gmail.com>
2025-03-06 17:24:59 -0500

    added CTE to provide detailed analysis of known problematic claims with uniform format.

---
+++ scripts/validation/payment_split/queries/ctes/problem_claim_analysis.sql
@@ -0,0 +1,30 @@
+-- ProblemClaimAnalysis: Provides detailed analysis of known problematic claims with uniform format
+-- depends on: ProblemClaimDetails
+-- Date filter: Uses @start_date to @end_date
+
+-- Include dependent CTE
+<<include:problem_claim_details.sql>>
+
+ProblemClaimAnalysis AS (
+    -- Problem claim details with payment links
+    SELECT 
+        NULL as PayNum,
+        NULL as PayAmt,
+        NULL as PayDate,
+        pcd.split_count,
+        NULL as claimproc_count,
+        CAST(pcd.ClaimNum AS CHAR) as claim_nums,
+        1 as has_known_oversplit_claims,
+        'Problem Claim' as payment_category,
+        NULL as total_split_amount,
+        NULL as split_difference,
+        NULL as splits_per_proc,
+        1 as is_suspicious,
+        'Claim' as record_type,
+        pcd.ClaimNum,
+        pcd.ClaimProcNum,
+        pcd.min_split_amt,
+        pcd.max_split_amt,
+        pcd.active_days
+    FROM ProblemClaimDetails pcd
+) 
