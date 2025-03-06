d8e89ba6d29b6093c9c163fc1e3960a984078b2b
Benjamin Rains <rains.bp@gmail.com>
2025-03-06 17:16:42 -0500

    removed dependent CTEs to separate files and added a "include directive" instead.

--- scripts/validation/payment_split/queries/duplicate_joins.sql
+++ scripts/validation/payment_split/queries/duplicate_joins.sql
@@ -12,66 +12,10 @@ Key metrics:
 - Known problematic claim detection
 - Split pattern suspicion scoring
 */
-
--- Note: CTEs from ctes.sql are automatically prepended by the export script
 -- Date filter: Use @start_date to @end_date variables
-
-, PaymentSplitAnalysis AS (
-    -- Base payment split analysis
-    SELECT 
-        ps.PayNum,
-        ps.PayAmt,
-        ps.PayDate,
-        ps.split_count,
-        cm.claimproc_count,
-        cm.claim_nums,
-        CASE WHEN cm.common_claim_count > 0 THEN 1 ELSE 0 END as has_known_oversplit_claims,
-        ps.payment_category,
-        ps.total_split_amount,
-        ps.split_difference,
-        ROUND(ps.split_count / NULLIF(cm.claimproc_count, 0), 1) as splits_per_proc,
-        CASE WHEN (
-            cm.common_claim_count = cm.total_claim_count 
-            AND cm.common_claim_count = 3 
-            AND ps.split_count / cm.total_claim_count > 20
-        ) OR (
-            ps.split_count > 100 
-            AND cm.total_claim_count < 5
-        ) THEN 1 ELSE 0 END as is_suspicious,
-        'Payment' as record_type,
-        NULL as ClaimNum,
-        NULL as ClaimProcNum,
-        NULL as min_split_amt,
-        NULL as max_split_amt,
-        NULL as active_days
-    FROM PaymentLevelMetrics ps
-    JOIN ClaimMetrics cm ON ps.PayNum = cm.PayNum
-    WHERE ps.split_count > 10
-),
-
-ProblemClaimAnalysis AS (
-    -- Problem claim details with payment links
-    SELECT 
-        NULL as PayNum,
-        NULL as PayAmt,
-        NULL as PayDate,
-        pcd.split_count,
-        NULL as claimproc_count,
-        CAST(pcd.ClaimNum AS CHAR) as claim_nums,
-        1 as has_known_oversplit_claims,
-        'Problem Claim' as payment_category,
-        NULL as total_split_amount,
-        NULL as split_difference,
-        NULL as splits_per_proc,
-        1 as is_suspicious,
-        'Claim' as record_type,
-        pcd.ClaimNum,
-        pcd.ClaimProcNum,
-        pcd.min_split_amt,
-        pcd.max_split_amt,
-        pcd.active_days
-    FROM ProblemClaimDetails pcd
-)
+-- Include CTEs
+<<include:ctes/payment_split_analysis.sql>>
+<<include:ctes/problem_claim_analysis.sql>>
 
 SELECT * FROM (
     SELECT * FROM PaymentSplitAnalysis
