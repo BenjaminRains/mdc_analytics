595ea2ff51b484a7d13bb17b3d030b9934239bd9
Benjamin Rains <rains.bp@gmail.com>
2025-03-06 21:15:34 -0500

    renamed the directory that contains the ddl files "opendental_db_schemas"

--- docs/opendental_schemas/payperiod_ddl.sql
+++
@@ -1,9 +0,0 @@
--- opendental_analytics_opendentalbackup_01_03_2025.payperiod definition
-
-CREATE TABLE `payperiod` (
-  `PayPeriodNum` bigint(20) NOT NULL AUTO_INCREMENT,
-  `DateStart` date NOT NULL DEFAULT '0001-01-01',
-  `DateStop` date NOT NULL DEFAULT '0001-01-01',
-  `DatePaycheck` date NOT NULL DEFAULT '0001-01-01',
-  PRIMARY KEY (`PayPeriodNum`)
-) ENGINE=MyISAM AUTO_INCREMENT=151 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_uca1400_ai_ci;
