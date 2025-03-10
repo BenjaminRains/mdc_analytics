dd23767ed1cf9f124409d5df4bffadc25fe531a1
Benjamin Rains <rains.bp@gmail.com>
2025-03-10 13:04:52 -0400

    changed reference to "payment_base_counts" to "payment_system_summary"

--- scripts/validation_development/payment_process/queries/payment_split/queries/base_counts.sql
+++ scripts/validation_development/payment_process/queries/payment_split/queries/base_counts.sql
@@ -1,14 +1,14 @@
-{% include 'payment_base_counts.sql' %}
+{% include 'payment_system_summary.sql' %}
 SELECT 
-    pbc.metric,
-    pbc.total_payments,
+    pss.metric,
+    pss.total_payments,
     COUNT(DISTINCT ps.SplitNum) as total_splits,
     COUNT(DISTINCT ps.ProcNum) as total_procedures,
     CAST(COUNT(DISTINCT ps.SplitNum) AS FLOAT) / 
         NULLIF(pbc.total_payments, 0) as avg_splits_per_payment,
     pbc.min_date,
-    pbc.max_date
-FROM PaymentBaseCounts pbc
+    pss.max_date
+FROM PaymentSystemSummary pss
 JOIN payment p ON p.PayDate >= @start_date AND p.PayDate < @end_date
 LEFT JOIN paysplit ps ON p.PayNum = ps.PayNum
 GROUP BY 
