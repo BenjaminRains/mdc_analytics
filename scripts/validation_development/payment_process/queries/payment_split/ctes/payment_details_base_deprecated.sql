521013c5911ca036deb8e6d7debfc47be38effd5
Benjamin Rains <rains.bp@gmail.com>
2025-03-10 13:06:22 -0400

    deprecated file

---
+++ scripts/validation_development/payment_process/queries/payment_split/ctes/payment_details_base_deprecated.sql
@@ -0,0 +1,17 @@
+PaymentDetailsBase AS (
+    SELECT 
+        p.PayNum,
+        p.PayDate,
+        p.PayType,
+        p.PayAmt,
+        p.PayNote,
+        ps.SplitNum,
+        ps.SplitAmt,
+        ps.ProcNum,
+        cp.ClaimNum
+    FROM payment p
+    JOIN paysplit ps ON p.PayNum = ps.PayNum
+    JOIN claimproc cp ON ps.ProcNum = cp.ProcNum
+    JOIN claim c ON cp.ClaimNum = c.ClaimNum
+    WHERE p.PayDate >= @start_date AND p.PayDate < @end_date
+)
