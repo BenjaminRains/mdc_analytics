b9b174e650ba5153c7e3064bd01decd0d04dc6a1
Benjamin Rains <rains.bp@gmail.com>
2025-03-10 13:09:06 -0400

    cte_macros file to help with WITH and ',' insertions for us with jinja environments

--- scripts/validation_development/payment_process/queries/payment_split/ctes/cte_macros.sql
+++ scripts/validation_development/payment_process/queries/payment_split/ctes/cte_macros.sql
@@ -1,7 +1,20 @@
-{% macro begin_ctes() %}
+{# Starts a CTE block with the WITH keyword - use at the beginning of any query containing CTEs #}
+{% macro begin_ctes() -%}
 WITH 
-{% endmacro %}
+{%- endmacro %}
 
-{% macro include_cte(cte_file, is_last=False) %}
-{% include cte_file %}{% if not is_last %},{% endif %}
-{% endmacro %}
+{# Includes a CTE file with proper comma handling - use is_first=True only for the first CTE #}
+{% macro include_cte(cte_file, is_first=False) -%}
+{%- if not is_first -%}
+,
+{% endif -%}
+{% include cte_file %}
+{%- endmacro %}
+
+{# Adds a comma between inline CTEs - use is_last=True for the last CTE in a WITH clause #}
+{% macro cte_separator(is_last=False) -%}
+{%- if not is_last %}
+,
+{% else %}
+{% endif -%}
+{%- endmacro %}
