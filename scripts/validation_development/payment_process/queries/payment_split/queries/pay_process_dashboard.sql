70b0cb5c672ff52a2a9d04fdbc24b268415212e3
Benjamin Rains <rains.bp@gmail.com>
2025-03-10 13:03:51 -0400

    created query to use the pay_process_funnel cte to create a payment processing health dashboard

---
+++ scripts/validation_development/payment_process/queries/payment_split/queries/pay_process_dashboard.sql
@@ -0,0 +1,18 @@
+{% include "pay_process_funnel.sql" %}
+-- Payment processing health dashboard - displays key metrics and KPIs for monitoring payment data completeness and integrity
+SELECT 
+    base_count,
+    with_splits,
+    with_procedures,
+    with_insurance,
+    patient_payments as valid_patient_payments,
+    transfer_count as internal_transfers,
+    refund_count as payment_refunds,
+    base_count - with_splits as missing_splits,
+    with_splits - with_procedures as unlinked_procedures,
+    avg_splits_per_payment,
+    mismatch_count as split_amount_mismatches,
+    ROUND(with_insurance * 100.0 / base_count, 1) as pct_insurance,
+    ROUND(patient_payments * 100.0 / base_count, 1) as pct_patient,
+    ROUND(transfer_count * 100.0 / base_count, 1) as pct_transfer
+FROM PayProcessFunnel;
