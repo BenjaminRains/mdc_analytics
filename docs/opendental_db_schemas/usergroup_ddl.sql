92d2094892568901ecb5910d3ec8db882db5f0af
Benjamin Rains <rains.bp@gmail.com>
2025-03-12 21:01:15 -0400

    moved documents to dbt_dental_clinic/docs

--- docs/opendental_db_schemas/usergroup_ddl.sql
+++
@@ -1,9 +0,0 @@
--- opendental_analytics_opendentalbackup_02_28_2025.usergroup definition
-
-CREATE TABLE `usergroup` (
-  `UserGroupNum` bigint(20) NOT NULL AUTO_INCREMENT,
-  `Description` varchar(255) DEFAULT '',
-  `UserGroupNumCEMT` bigint(20) NOT NULL,
-  PRIMARY KEY (`UserGroupNum`),
-  KEY `UserGroupNumCEMT` (`UserGroupNumCEMT`)
-) ENGINE=MyISAM AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_general_ci;
